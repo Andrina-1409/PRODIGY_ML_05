26b2688380420dbfcaedc5f2dfaf8740e6079eb3
Andrina-1409 <andrinakjohnaiml2023@jerusalemengg.ac.in>
2025-07-02 21:47:51 +0530

    Update food_train.py

--- food_train.py
+++ food_train.py
@@ -1,45 +1,50 @@
+from tensorflow.keras.models import Model
 import os
-import cv2
-import numpy as np
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
-from tensorflow.keras.utils import to_categorical
+import numpy
 from sklearn.model_selection import train_test_split
+import cv2
+from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import numpy as np
+from tensorflow.keras.utils import to_categorical
+from tensorflow.keras.applications import MobileNetV2
+from tensorflow.keras.layers import Dropout ,GlobalAveragePooling2D,Dense
 
-a = r'C:\Users\johnd\OneDrive\Desktop\internship\task5\images'
-b = sorted(os.listdir(a))
-c = len(b)
-d, e = [], []
-f = (100*2)
-
-for g in range(c):
-    h = os.path.join(a, b[g])
-    i = os.listdir(h)[:f]
-    for j in i:
-        k = os.path.join(h, j)
-        l = cv2.imread(k)
-        if l is not None:
-            m = cv2.resize(l, (64, 64))
-            d.append(m)
-            e.append(g)
-
-n = np.array(d).astype('float32') / (5*51)
-o = to_categorical(np.array(e), num_classes=c)
-p, q, r, s = train_test_split(n, o, test_size=(5*4)/100, random_state=(80+2))
-
-t = Sequential()
-t.add(Conv2D((16*2), (3, 3), activation='relu', input_shape=(64, 64, 3)))
-t.add(MaxPooling2D((2, 2)))
-t.add(Conv2D((30*2)+4, (3, 3), activation='relu'))
-t.add(MaxPooling2D((2, 2)))
-t.add(Flatten())
-t.add(Dense((64*2), activation='relu'))
-t.add(Dense(c, activation='softmax'))
-
-t.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
-u = t.fit(p, r, epochs=((7*2)-4), validation_data=(q, s))
-
-v, w = t.evaluate(q, s)
-print("End Score ~", round(w*100, 2), "%")
-t.save('food_classifier.h5')
-print("Export Finished")
+path=r'D:\Python projects\foodcalorieCNN\images'
+cat=[]
+lst=os.listdir(path)
+for xx in lst:
+ loc=path+'\\'+xx
+ if os.path.isdir(loc):
+  cat.append(xx)
+cat.sort()
+cl=len(cat)
+img,lbl=[],[]
+for ind,val in enumerate(cat):
+ pth=os.path.join(path,val)
+ temp=os.listdir(pth)[:150]  
+ for ele in temp:
+  full=os.path.join(pth,ele)
+  im=cv2.imread(full)
+  if im is not None:
+   im=cv2.resize(im,(123,123))
+   img.append(im)
+   lbl.append(ind)
+arr=np.array(img).astype('float32')/255
+lab=to_categorical(np.array(lbl),num_classes=cl)
+a1,a2,a3,a4=train_test_split(arr,lab,test_size=0.23,random_state=7)
+dat=ImageDataGenerator(rotation_range=17,zoom_range=0.13,width_shift_range=0.07,height_shift_range=0.06,horizontal_flip=True)
+dat.fit(a1)
+base=MobileNetV2(weights='imagenet',include_top=False,input_shape=(123,123,3))
+for lay in base.layers: lay.trainable=False
+out=base.output
+out=GlobalAveragePooling2D()(out)
+out=Dense(173,activation='relu')(out)
+out=Dropout(0.29)(out)
+out=Dense(cl,activation='softmax')(out)
+mod=Model(inputs=base.input,outputs=out)
+mod.compile(optimizer='adam',loss='categorical_crossentropy',metrics=['accuracy'])
+h=mod.fit(dat.flow(a1,a3,batch_size=29),epochs=19,validation_data=(a2,a4))
+l,s=mod.evaluate(a2,a4)
+print('Acc:',round(s*100,2))
+mod.save('food_classifier.h5')
+print('Finished')
